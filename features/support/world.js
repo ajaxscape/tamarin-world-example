28b8ff9a0401d350fddd3068784c89fa6c8f274d
Ben on windows <ben@ajaxscape.com>
2016-06-15 20:01:05 +0100

    Things looking good now

--- features/support/world.js
+++ features/support/world.js
@@ -3,17 +3,17 @@
 const webDriver = require('selenium-webdriver')
 const chrome = require('selenium-webdriver/chrome')
 
-const service = new chrome.ServiceBuilder(require('chromedriver').path).build()
-chrome.setDefaultService(service)
-
-const driver = new webDriver.Builder()
-  .withCapabilities(webDriver.Capabilities.chrome())
-  .build()
-
 module.exports = {
   World: class extends require('tamarin-world').World {
-    getDriver () {
-      return super.getDriver(driver)
+    constructor () {
+      const service = new chrome.ServiceBuilder(require('chromedriver').path).build()
+      chrome.setDefaultService(service)
+
+      const driver = new webDriver.Builder()
+        .withCapabilities(webDriver.Capabilities.chrome())
+        .build()
+
+      super(driver)
     }
   }
 }
