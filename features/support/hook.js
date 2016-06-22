98c41e40118ea12f813d97c406876e4c769eab73
Ben on windows <ben@ajaxscape.com>
2016-06-22 06:46:31 +0100

    Sorted out driver issue

--- features/support/hook.js
+++ features/support/hook.js
@@ -1,6 +1,11 @@
 'use strict'
 
 module.exports = function () {
+  const driver = require('./world').driver
   this.World = require('./world').World // overwrite default World constructor
   this.setDefaultTimeout(3600000)
+
+  this.AfterFeatures(function () {
+    return driver.quit()
+  })
 }
--- features/support/world.js
+++ features/support/world.js
@@ -2,19 +2,19 @@
 
 const webDriver = require('selenium-webdriver')
 const chrome = require('selenium-webdriver/chrome')
+const service = new chrome.ServiceBuilder(require('chromedriver').path).build()
+chrome.setDefaultService(service)
+
+const driver = new webDriver.Builder()
+  .withCapabilities(webDriver.Capabilities.chrome())
+  .build()
 
 module.exports = {
   World: class extends require('tamarin-world').World {
     constructor () {
-      const service = new chrome.ServiceBuilder(require('chromedriver').path).build()
-      chrome.setDefaultService(service)
-
-      const driver = new webDriver.Builder()
-        .withCapabilities(webDriver.Capabilities.chrome())
-        .build()
-
       super(driver)
     }
-  }
+  },
+  driver: driver
 }
 
