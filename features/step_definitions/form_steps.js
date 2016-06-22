ae0d65f601d39bb373387bb8eaf0e31b74aedf66
Ben on windows <ben@ajaxscape.com>
2016-06-22 07:26:28 +0100

    Attempt at using xpath

--- features/step_definitions/form_steps.js
+++ features/step_definitions/form_steps.js
@@ -1,5 +1,8 @@
 'use strict'
 
+const webDriver = require('selenium-webdriver')
+const By = webDriver.By
+
 require('chai')
   .use(require('chai-as-promised'))
   .should()
@@ -15,17 +18,21 @@ module.exports = function () {
   })
 
   this.When(/^I click the "([^"]*)" menu link$/, function (linkText) {
-    return this.whenReady('#top_nav', 5)
-      .then((nav) => this.executeScript(($nav, $linkText) => {
-        let results = Array.prototype.slice.call($nav.querySelectorAll('a'))
-        let link = results.filter((el) => el.text === $linkText).pop()
-        if (link) {
-          link.click()
-        } else {
-          throw new Error('Cannot find link: ' + $linkText)
-        }
-      }, nav, linkText))
+    return this.click(By.xpath(`//a[contains(.,'${linkText}')]`), 5)
   })
+  //
+  //this.When(/^I click the "([^"]*)" menu link$/, function (linkText) {
+  //  return this.whenReady('#top_nav', 5)
+  //    .then((nav) => this.executeScript(($nav, $linkText) => {
+  //      let results = Array.prototype.slice.call($nav.querySelectorAll('a'))
+  //      let link = results.filter((el) => el.text === $linkText).pop()
+  //      if (link) {
+  //        link.click()
+  //      } else {
+  //        throw new Error('Cannot find link: ' + $linkText)
+  //      }
+  //    }, nav, linkText))
+  //})
 
   this.Then(/^I expect to see some "([^"]*)" results$/, function (type) {
     return this.getData('searchTerm')
