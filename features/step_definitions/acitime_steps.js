72ad311401e5e024f4b80ba2bd3a65cbf01fd226
ajaxscape <ben@ajaxscape.com>
2016-06-28 11:55:11 +0100

    Rename find to page

--- features/step_definitions/acitime_steps.js
+++ features/step_definitions/acitime_steps.js
@@ -7,7 +7,7 @@ require('chai')
   .use(require('chai-as-promised'))
   .should()
 
-const find = {
+const page = {
   'Features': By.xpath('//*[@id="nav-features"]/a'),
   'subMenu': (linkText) => By.xpath(`//*[@id="nav-features"]//a[text()="${linkText}"]`),
   'heading': By.css('h1')
@@ -18,14 +18,14 @@ const hoverDelay = 500
 
 module.exports = function () {
   this.When(/^I hover over the "([^"]*)" menu link$/, function (link) {
-    return this.hover(find[link], hoverDelay, retries)
+    return this.hover(page[link], hoverDelay, retries)
   })
 
   this.When(/^I click the "([^"]*)" submenu link$/, function (linkText) {
-    return this.click(find.subMenu(linkText), retries)
+    return this.click(page.subMenu(linkText), retries)
   })
 
   this.Then(/^I expect the heading to be "([^"]*)"$/, function (text) {
-    return this.getText(find.heading, retries).should.eventually.equal(text)
+    return this.getText(page.heading, retries).should.eventually.equal(text)
   })
 }
--- features/step_definitions/google_steps.js
+++ features/step_definitions/google_steps.js
@@ -3,7 +3,7 @@
 const webDriver = require('selenium-webdriver')
 const By = webDriver.By
 
-const find = {
+const page = {
   'search': By.css('[title="Search"]'),
   'navLink': (linkText) => By.xpath(`//*[@role="navigation"]//a[text()="${linkText}"]`),
   'results': (type, searchTerm) => By.css(`img[alt="${type} result for ${searchTerm}"]`)
@@ -14,15 +14,15 @@ const retries = 5
 module.exports = function () {
   this.When(/^I search for "([^"]*)"$/, function (searchTerm) {
     return this.setData('searchTerm', searchTerm)
-      .then(() => this.sendKeys(find.search, searchTerm + '\n', retries))
+      .then(() => this.sendKeys(page.search, searchTerm + '\n', retries))
   })
 
   this.When(/^I click the "([^"]*)" menu link$/, function (linkText) {
-    return this.click(find.navLink(linkText), retries)
+    return this.click(page.navLink(linkText), retries)
   })
 
   this.Then(/^I expect to see some "([^"]*)" results$/, function (type) {
     return this.getData('searchTerm')
-      .then((searchTerm) => this.whenReady(find.results(type, searchTerm), retries))
+      .then((searchTerm) => this.whenReady(page.results(type, searchTerm), retries))
   })
 }
