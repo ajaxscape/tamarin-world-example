724e029744a483b5db77b7de7254165d7ee6cbff
Ben on windows <ben@ajaxscape.com>
2016-10-18 18:57:08 +0100

    Support waiting for cookies to be set

--- features/actitime.feature
+++ features/actitime.feature
@@ -4,6 +4,8 @@ Feature: Explore Actitime
 
   Scenario: Actitime
     Given I visit https://www.actitime.com/
+    Then I expect the "_ym_isad" cookie to be "2"
+    And I expect the "_ym_uidd" cookie to exist
     When I hover over the "Features" menu link
     And I click the "Features in detail" submenu link
     Then I expect the heading to be "Features in Detail"
--- features/step_definitions/form_steps.js
+++ features/step_definitions/form_steps.js
@@ -8,4 +8,12 @@ module.exports = function () {
   this.Then(/^I expect the title to be "([^"]*)"$/, function waitForTitleStep (title) {
     return this.waitForTitle(title)
   })
+
+  this.Then(/^I expect the "([^"]*)" cookie to exist$/, function waitForTitleStep (name) {
+    return this.waitForCookie(name)
+  })
+
+  this.Then(/^I expect the "([^"]*)" cookie to be "([^"]*)"$/, function waitForTitleStep (name, expectedValue) {
+    return this.waitForCookie(name).should.eventually.have.property('value', expectedValue)
+  })
 }
