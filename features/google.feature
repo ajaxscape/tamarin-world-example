ede9cf62185052cb34224307746f652aad850c13
Ben on windows <ben@ajaxscape.com>
2016-10-31 07:20:52 +0000

    Update to new tamarin-world

--- features/google.feature
+++ features/google.feature
@@ -2,7 +2,7 @@ Feature: Do a Google Search
   Using a web browser
   I want to perform a Google search
 
-  Scenario: Google Search
+  Scenario: Google Search for tamarin images
     Given I visit http://google.com
     Then I expect the title to be "Google"
     When I search for "Tamarin"
--- features/step_definitions/google_steps.js
+++ features/step_definitions/google_steps.js
@@ -1,28 +1,23 @@
 'use strict'
 
-const webDriver = require('selenium-webdriver')
-const By = webDriver.By
-
 const page = {
-  'search': By.css('[title="Search"]'),
-  'navLink': (linkText) => By.xpath(`//*[@role="navigation"]//a[text()="${linkText}"]`),
-  'results': (type, searchTerm) => By.css(`img[alt="${type} result for ${searchTerm}"]`)
+  'search': { css: '[title="Search"]' },
+  'navLink': (linkText) => ({ xpath: `//*[@role="navigation"]//a[text()="${linkText}"]` }),
+  'results': (type, searchTerm) => ({ css: `img[alt="${type} result for ${searchTerm}"]` })
 }
-
-const retries = 5
 
 module.exports = function () {
   this.When(/^I search for "([^"]*)"$/, function enterSearchTermStep (searchTerm) {
     return this.setData('searchTerm', searchTerm)
-      .then(() => this.sendKeys(page.search, searchTerm + '\n', retries))
+      .then(() => this.sendKeys(page.search, searchTerm + '\n'))
   })
 
   this.When(/^I click the "([^"]*)" menu link$/, function clickMenuLinkStep (linkText) {
-    return this.click(page.navLink(linkText), retries)
+    return this.click(page.navLink(linkText))
   })
 
   this.Then(/^I expect to see some "([^"]*)" results$/, function waitForResultsStep (type) {
     return this.getData('searchTerm')
-      .then((searchTerm) => this.waitFor(page.results(type, searchTerm), retries))
+      .then((searchTerm) => this.waitFor(page.results(type, searchTerm)))
   })
 }
